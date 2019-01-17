f99f4ac52f9f08ccd7bd7e50c316ca70e96d4c11
AbelardoRivas <abelardo.rivas1@gmail.com>
2019-01-17 08:44:53 -0400

    Rutas anidadas

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import axios from './../../../axios';
 import Post from './../../../components/Post/Post';
 import './Posts.css';
+import { Route } from 'react-router-dom';
+import FullPost from './../FullPost/FullPost';
 //import { Link } from 'react-router-dom';
 
 class Posts extends Component {
@@ -28,14 +30,14 @@ class Posts extends Component {
   }
 
   postSelectedHandler = (id) => {
-    this.props.history.push({pathname: '/' + id});
-    //this.props.history.push({'/' + id});
+    this.props.history.push({pathname: '/posts/' + id});
+    //this.props.history.push({'/posts/' + id});
   }
 
   render () {
     const posts = this.state.posts.map(post => {
       return (
-        //<Link to={'/' + post.id} key={post.id}>
+        //<Link to={'/posts/' + post.id} key={post.id}>
           <Post
           key={post.id}
           title={post.title}
@@ -46,9 +48,12 @@ class Posts extends Component {
     });
 
     return (
-      <section className="Posts">
-          {posts}
-      </section>
+      <div>
+        <section className="Posts">
+            {posts}
+        </section>
+        <Route path={this.props.match.url + '/:id'} exact component={FullPost} />
+      </div>
     );
   }
 }
